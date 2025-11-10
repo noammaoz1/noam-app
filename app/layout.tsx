6b652a5cb9989833dcdf49af5d1897e9526cc9b8
Noam Maoz <noam.maoz1@mail.huji.ac.il>
2025-11-10 23:08:27 +0200

    Changed the content, the logo and added tic-tac-toe game card to the home page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import Navbar from "@/lib/components/Navbar";
 import Footer from "@/lib/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Digital Product Jam Starter Kit",
-  description:
-    "A starter kit for wiritng code in the Digital Product Jam course.",
+  title: "Noam's App",
+  // description:
+  //   "A starter kit for wiritng code in the Digital Product Jam course.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -16,19 +16,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html>
       <head>
         {/* Browser Favicon */}
-        <link rel="icon" href="/icons/favicon.png" />
+        <link rel="icon" href="/icons/n-logo.png" />
         {/* Apple Icon */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/icon-180.png"
+          href="/icons/n-logo.png"
         />
         {/* Android Icon */}
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/icons/icon-192.png"
+          href="/icons/n-logo.png"
         />
         <link rel="manifest" href="/manifest.json" />
         <script src="https://accounts.google.com/gsi/client" async></script>
--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -4,9 +4,8 @@ import { AuthAction } from "./AuthAction";
 export default function Footer() {
   return (
     <footer>
-      <p>This is some example footer content.</p>
       <p>
-        <AuthAction /> | {COURSE_CREDITS}
+        <AuthAction /> Noam Maoz | {COURSE_CREDITS}
       </p>
     </footer>
   );
